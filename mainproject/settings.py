71a2a9afa7b1b69e1728412e96335e52762b7692
sarad <risingstar3993@gmail.com>
2020-07-24 13:16:31 +0545

    Changes Made

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djapp'
+    'djapp',
+    'djproduct',
 ]
 
 MIDDLEWARE = [
