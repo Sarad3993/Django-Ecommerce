5103836e5f0ae4ba78eeb9b9a25c99d3f3fe02b6
sarad <risingstar3993@gmail.com>
2020-07-24 14:40:48 +0545

    models for category and products created and registered

--- djproduct/admin.py
+++ djproduct/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from djproduct.models import *
+
+# Register models here 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','parent','status']
+    list_filter = ['status']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','category','status'] # list is displayed on the basis of above 3 categories 
+    list_filter = ['category'] # list is filtered on the basis of this filed category 
+
+admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Product,ProductAdmin)
--- djproduct/models.py
+++ djproduct/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+# model for category 
+class Category(models.Model):
+    STATUS = (('True', 'True'),('False', 'False'))
+    parent = models.ForeignKey('self',blank=True, null=True,related_name='children', on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=300)
+    description = models.TextField(max_length=300)
+    image=models.ImageField(upload_to='images/',blank=True)
+    status=models.CharField(choices=STATUS,max_length=100)
+    slug = models.SlugField(null=False, unique=True)
+
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+# model for products/items  
+
+class Product(models.Model):
+    STATUS = (('True', 'True'),('False', 'False'))
+    # Many to one relation 
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=300)
+    description = models.TextField(max_length=300)
+    image=models.ImageField(upload_to='images/', blank=True) 
+    price = models.FloatField()
+    amount=models.IntegerField()
+    min_amount=models.IntegerField()
+    details=models.TextField()
+    slug = models.SlugField()
+    status=models.CharField(max_length=100,choices=STATUS) 
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
