e69c3d16061f7c858bd6d05da0ebf69319c5c6ca
sarad <risingstar3993@gmail.com>
2020-08-07 10:05:52 +0545

    Password reset working perfect now

--- djuser/urls.py
+++ djuser/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from djuser.views import *
-from django.contrib.auth import views as auth_views
+
+
 app_name = 'djuser'
 
 urlpatterns = [
@@ -10,19 +11,7 @@ urlpatterns = [
     path('signup/',signup_form,name='signup'),
     path('update_profile/',user_profile_update,name='update_profile'),
     path('change_password/',user_password_change,name='change_password'),
-
-        # for password reset 
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
-         name="password_reset"),
-
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
-         name="password_reset_done"),
-
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
-         name="password_reset_confirm"),
-
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
-         name="password_reset_complete"),
+    
 ]
-]
+
 
--- mainproject/urls.py
+++ mainproject/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -25,6 +26,19 @@ urlpatterns = [
     path('product/', include('djproduct.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('user/', include('djuser.urls')),
+
+    # for password reset 
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name="password_reset"),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name="password_reset_confirm"),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+         name="password_reset_complete"),
 ]
 
 # we have to add this if we want images(media) to display in admin panel...for that we have to import settings and static from django which is done above
