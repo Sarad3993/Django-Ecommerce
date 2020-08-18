38c2a290d9c2e93d4965a8233b0a838b1ca063fb
Sarad3993 <risingstar3993@gmail.com>
2020-08-18 14:06:33 +0545

    first commit

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5=-w205$5c(+3x79xqrwbo6*#ho@q*091cshl7mabo=s8x$2z7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sarad-ecommerce.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
